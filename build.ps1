5a1bc7e3662fa885a108a842cf059eba3a9c3db6
Carlos dos Santos <cds@cds-software.com.br>
2017-04-10 15:26:21 -0300

    build

---
+++ build.ps1
@@ -0,0 +1,23 @@
+ $rootDir = $env:APPVEYOR_BUILD_FOLDER
+ $buildNumber = $env:APPVEYOR_BUILD_NUMBER
+ $solutionFile = "$rootDir\CDSImprimeTexto.sln"
+ $srcDir = "$rootDir\nuget\CDSImprimeTexto.net"
+ $slns = ls "$rootDir\*.sln"
+ $packagesDir = "$rootDir\packages"
+ $nuspecPath = "$rootDir\nuget\boleto.net\boleto.net.nuspec"
+ $nugetExe = "$packagesDir\NuGet.CommandLine.3.5.0\tools\NuGet.exe"
+ $nupkgPath = "$rootDir\src\NuGet\CDSImprimeTexto.{0}.nupkg"
+
+foreach($sln in $slns) {
+   nuget restore $sln
+}
+
+[xml]$xml = cat $nuspecPath
+$xml.package.metadata.version+=".$buildNumber"
+$xml.Save($nuspecPath)
+
+[xml]$xml = cat $nuspecPath
+$nupkgPath = $nupkgPath -f $xml.package.metadata.version
+
+nuget pack $nuspecPath -properties "Configuration=$env:configuration;Platform=AnyCPU;Version=$($env:appveyor_build_version)" -OutputDirectory $srcDir 
+appveyor PushArtifact $nupkgPath
